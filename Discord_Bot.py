a3d401d7a9201e54f272e14518f25b919a4f33c1
djnf <djnf@NICO-PC>
2022-05-27 19:11:56 +0200

    format log as code

--- Discord_Bot.py
+++ Discord_Bot.py
@@ -23,12 +23,12 @@ async def on_voice_state_update(member, before, after):
     str_date_time = dt.strftime("%d-%m-%Y, %H:%M:%S")
     channel = client.get_channel(962432876475998298)
     if after.channel is None and before.channel is not None:
-        await channel.send(str_date_time + " " + member.name + " verliess den Channel " + before.channel.name)
+        await channel.send("`" + str_date_time + " " + member.name + " verliess den Channel " + before.channel.name + "`")
     if before.channel is None and after.channel is not None:
-        await channel.send(str_date_time + " " + member.name + " betrat den Channel " + after.channel.name)    
+        await channel.send("`" + str_date_time + " " + member.name + " betrat den Channel " + after.channel.name + "`")    
     if before.channel is not None and after.channel is not None:
         if after.channel.name != before.channel.name:
-            await channel.send(str_date_time + " " + member.name + " ging von " + before.channel.name + " nach " + after.channel.name)
+            await channel.send("`" + str_date_time + " " + member.name + " ging von " + before.channel.name + " nach " + after.channel.name + "`")
  
 
 from pathlib import Path
